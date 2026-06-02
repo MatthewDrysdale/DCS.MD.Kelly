84d3fe80b2f5fcdbd8ffc3f808249461b9abceef
Matthew <matthewdrysdale98@outlook.com>
2026-06-02 15:49:46 +0100

    Removing logger && re-implementing authorizedview

--- DCS.MD.Kelly.Web/Program.cs
+++ DCS.MD.Kelly.Web/Program.cs
@@ -1,6 +1,5 @@
 using DCS.MD.Kelly.Web;
 using DCS.MD.Kelly.Web.Services;
-using DCS.MD.Logging;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
@@ -9,14 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
-#region Logger Setup
-LoggerConfig? loggerConfig = builder.Configuration.GetSection("LoggerConfig").Get<LoggerConfig>();
-
-Logger logger = DCS.MD.Logging.LoggerFactory.CreateFromConfig(loggerConfig!);
-
-builder.Services.AddSingleton(logger);
-#endregion
 
 builder.Services.AddScoped<AuthService>();
 
